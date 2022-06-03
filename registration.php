e5301dfea6a508a9ddf17d59375aeacb97668ba7
Ariya InfoTech <toariyainfotech@gmail.com>
2022-06-03 15:07:54 +0530

    module upload

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'AriyaInfoTech_HSNCode', __DIR__);
+
